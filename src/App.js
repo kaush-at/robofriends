71cd91fdbfc715723f4d06bdb5de690b0ba187b9
kaush <emailtokaush@gmail.com>
2019-12-20 19:11:16 -0500

    modification added to the project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
-import React from 'react';
+//import React from 'react';  // we need to extends React.Component
+import React, {Component} from 'react';
 import CardList from './CardList';
 import { robots } from './robots';
 import SearchBox from './SearchBox';
+import './App.css';
 
-const App = () => {
-    return (
-        <div className='tc'>
-            <h1>RoboFriends</h1>
-            <SearchBox></SearchBox>
-            <CardList robots={robots} />
-        </div>
-    );
+
+// 
+class App extends Component{
+    constructor(){
+    super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+            }
+    }
+
+    onsearchChange =(event) =>{
+        this.setState({searchfield : event.target.value});
+    }
+
+    render(){
+
+    const filteredRobots = this.state.robots.filter(robots => {
+        return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    });
+        return (
+            <div className='tc'>
+                <h1 className="f1">RoboFriends</h1>
+                <SearchBox searchChange ={this.onsearchChange}/>
+                <CardList robots={filteredRobots} />
+            </div>
+        );
+    }
+    
 }
 
 export default App;
